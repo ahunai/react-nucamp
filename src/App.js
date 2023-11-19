c3df97b5dfbe5da11986f52928f88ce7a11576f8
Isaac <ahunai@hawaii.edu>
2023-11-18 16:50:41 -1000

    End of Workshop 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React from 'react';
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Header from "./components/Header.js"
+import Footer from './components/Footer.js';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-          <Container>
-              <NavbarBrand href='/'>
-                  <img src={NucampLogo} alt='nucamp logo' />
-              </NavbarBrand>
-          </Container>
-      </Navbar>
-      I'm Ready for the Workshop!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
