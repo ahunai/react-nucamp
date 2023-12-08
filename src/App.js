1c5ebfb5f1199cd9040c4639d2adced3e4ec3695
Isaac <ahunai@hawaii.edu>
2023-12-08 09:43:07 -1000

    End of Week 2 Workshop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import './App.css';
-import CampsitesList from './features/campsites/CampsitesList.js';
-import Header from "./components/Header.js"
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage.js";
+import HomePage from "./pages/HomePage.js";
+import Header from "./components/Header.js";
 import Footer from './components/Footer.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
+import CampsiteDetailPage from "./pages/CampsiteDetailPage.js";
+import AboutPage from "./pages/AboutPage.js";
+import './App.css';
+
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route 
+          path="directory/:campsiteId"
+          element={<CampsiteDetailPage />}
+        />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
       <Footer />
     </div>
   );
